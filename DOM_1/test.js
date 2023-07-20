a5ce67bfce03d497b0f15bbadc9cea50697c0ab6
aw3316 <64852634+aw3316@users.noreply.github.com>
2023-07-20 23:25:03 +0900

    Add files via upload
    출처: 유투브 뉴렉처

---
+++ DOM_1/test.js
@@ -0,0 +1,16 @@
+// window.onload = function(){ 
+window.addEventListener("load", function() {
+    alert("안녕하세요");
+});
+
+// ex19에 오류뜬다.
+  /*
+    
+    => window.addEventListener("load", function() {
+        alert("test1");
+
+    });
+
+    */
+    
+    
